3100687d3507c79e041b75b5383b98cfc790edd1
arosspope <andrew.pope456@gmail.com>
2017-06-13 11:30:31 +1000

    more sophisticated solution using recursion

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,26 +1,32 @@
-use std::io;
+use std::env;
 
 fn main() {
-    println!("Fibonacci sequence");
-    println!("How many elements of the sequence do you wish to generate? ");
-    let mut limit = String::new();
+    let args: Vec <_> = env::args().collect();
 
-    io::stdin().read_line(&mut limit)
-        .expect("Failed to read line");
-
-    let limit: u32 = limit.trim().parse()
-        .expect("That is not a valid number");
-
-    let mut f_n_2 = 0;
-    println!("fib[0]: {}", f_n_2);
-    let mut f_n_1 = 1;
-    println!("fib[1]: {}", f_n_1);
-
-    for i in 2..limit {
-        let f_i = f_n_1 + f_n_2;
-        f_n_2 = f_n_1;
-        f_n_1 = f_i;
-
-        println!("fib[{}]: {}", i, f_i);
+    if args.len() != 2 {
+        panic!("Usage: ./fibonacci <number of elements>");
     }
+
+    println!("~~~ Fibonacci Generator ~~~");
+    let limit: u64 = args[1].trim().parse()
+        .expect("That is not a valid unsigned number");
+
+    println!();
+
+    for i in 0..limit {
+        print!(" {}", fibonacci(i));
+    }
+
+    println!();
+}
+
+
+fn fibonacci(n: u64) -> u64 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    }
+
+    fibonacci(n - 1) + fibonacci(n - 2)
 }
